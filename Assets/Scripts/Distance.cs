c286ff388e948d40cdf2efa2006d04bd014c7669
jacklp <jackleonprior@gmail.com>
2016-04-02 19:17:40 +0100

    first commit

---
+++ Assets/Scripts/Distance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+//Help class to sort the distances
+//Requires "using System;" at the top
+public class Distance : IComparable<Distance> {
+	//The distance to water
+	public float distance;
+	//We also need to store a name so we can form clockwise triangles
+	public string name;
+	//The Vector3 position of the vertice
+	public Vector3 verticePos;
+
+	public int CompareTo(Distance other) {
+		return this.distance.CompareTo(other.distance);
+	}
+}
---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveController : MonoBehaviour {
+
+	public float scale = 0.1f;
+	public float speed = 1.0f;
+	//The width between the wave peaks
+	public float waveDistance = 1f;
+	//Noise parameters
+	public float noiseStrength = 1f;
+	public float noiseWalk = 1f;
+
+	public float GetWaveYPos(float x_coord, float z_coord) {
+		float y_coord = 0f;
+	
+		y_coord += Mathf.Sin((Time.time * speed + z_coord) / waveDistance) * scale;
+
+		//add noise to make it more realistic
+		y_coord += Mathf.PerlinNoise(x_coord + noiseWalk, z_coord + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+
+		return y_coord;
+	}	
+}
