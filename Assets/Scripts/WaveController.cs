63beac79a95a72ec85225d8202077cbdce4dad69
jacklp <jackleonprior@gmail.com>
2016-04-03 21:17:19 +0100

    added noise, and time controls to guy

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -4,6 +4,7 @@ using System.Xml;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using UnityEditor;
 
 public class WaveController : MonoBehaviour {
 
@@ -31,17 +32,17 @@ public class WaveController : MonoBehaviour {
 		labelStyle.font = f;
 		labelStyle.fixedWidth = 100;
 
-		sliderStyle = new GUIStyle(GUI.skin.horizontalSlider);
+		/*sliderStyle = new GUIStyle(GUI.skin.horizontalSlider);
 		sliderStyle.border.left = 2;
 		sliderStyle.border.bottom = 2;
 		sliderStyle.border.top = 2;
 		sliderStyle.border.right = 2;
 
 		sliderThumbStyle = new GUIStyle(GUI.skin.horizontalSliderThumb);
-
+		*/
 
 		//Begin Area
-		GUILayout.BeginArea(new Rect(0,0, Screen.width/4, (Screen.height*2)/3), backgroundStyle);
+		GUILayout.BeginArea(new Rect(0,0, Screen.width/4, (Screen.height*4)/5), backgroundStyle);
 
 		GUILayout.BeginHorizontal();
 		GUILayout.Label("Amplitude", labelStyle);
@@ -61,12 +62,55 @@ public class WaveController : MonoBehaviour {
 		GUILayout.EndHorizontal();
 		GUILayout.Space(15);
 
+		GUILayout.BeginHorizontal();
+
+		int noiseBoolVal = (int)sineWaveModel.modelData ["noiseBool"];
+		int noiseBool = GUILayout.SelectionGrid(noiseBoolVal, new string[] { "Noise Off", "Noise On"}, 1, EditorStyles.radioButton);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Noise Strength", labelStyle);
+		float noiseStrength = GUILayout.HorizontalSlider ( sineWaveModel.modelData["noiseStrength"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Noise Walk", labelStyle);
+		float noiseWalk = GUILayout.HorizontalSlider ( sineWaveModel.modelData["noiseWalk"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Bias", labelStyle);
+		float bias = GUILayout.HorizontalSlider ( sineWaveModel.modelData["bias"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		int continuousBoolVal = (int)sineWaveModel.modelData ["continuousBool"];
+		int continuousBool = GUILayout.SelectionGrid(continuousBoolVal, new string[] { "Time Period", "Continuous"}, 1, EditorStyles.radioButton);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Time Period", labelStyle);
+		float timePeriod = GUILayout.HorizontalSlider ( sineWaveModel.modelData["timePeriod"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
 		GUILayout.EndArea ();
 
 		if(GUI.changed){
 			sineWaveModel.modelData["amplitude"] = amplitude;
 			sineWaveModel.modelData["speed"] = speed;
 			sineWaveModel.modelData["wavelength"] = wavelength;
+			sineWaveModel.modelData ["noiseBool"] = (float)noiseBool;
+			sineWaveModel.modelData ["noiseStrength"] = noiseStrength;
+			sineWaveModel.modelData ["noiseWalk"] = noiseWalk;
+			sineWaveModel.modelData ["bias"] = bias;
+			sineWaveModel.modelData ["continuousBool"] = (float)continuousBool;
+			sineWaveModel.modelData ["timePeriod"] = timePeriod;
 		}
 	}
 		
