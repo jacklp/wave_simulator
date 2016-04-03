0cae9d6b08b876f8be5830e49dd9234b0275daa3
jacklp <jackleonprior@gmail.com>
2016-04-03 20:25:12 +0100

    beginning of gui set up

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -9,15 +9,69 @@ public class WaveController : MonoBehaviour {
 
 	SineWaveModel sineWaveModel;
 
-	void Start(){
+	private Font f;
+	GUIStyle backgroundStyle;
+	GUIStyle labelStyle;
+	GUIStyle sliderStyle;
+	GUIStyle sliderThumbStyle;
+
+	void Awake(){
+		f = (Font)Resources.Load("tattoosailor");
 		sineWaveModel = new SineWaveModel ();
 		sineWaveModel.buildModel ();
+	}
+
+	void OnGUI()
+	{
+		backgroundStyle = new GUIStyle();
+		//backgroundStyle.normal.background = MakeTex(600, 1, new Color(0f, 0f, 0f, 1.0f));
+
+		labelStyle = new GUIStyle(GUI.skin.label);
+		labelStyle.fontSize = 12;
+		labelStyle.font = f;
+		labelStyle.fixedWidth = 100;
+
+		sliderStyle = new GUIStyle(GUI.skin.horizontalSlider);
+		sliderStyle.border.left = 2;
+		sliderStyle.border.bottom = 2;
+		sliderStyle.border.top = 2;
+		sliderStyle.border.right = 2;
+
+		sliderThumbStyle = new GUIStyle(GUI.skin.horizontalSliderThumb);
+
+
+		//Begin Area
+		GUILayout.BeginArea(new Rect(0,0, Screen.width/4, (Screen.height*2)/3), backgroundStyle);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Amplitude", labelStyle);
+		float amplitude = GUILayout.HorizontalSlider ( sineWaveModel.modelData["amplitude"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Speed", labelStyle);
+		float speed = GUILayout.HorizontalSlider ( sineWaveModel.modelData["speed"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Wavelength", labelStyle);
+		float wavelength = GUILayout.HorizontalSlider ( sineWaveModel.modelData["wavelength"], 0.0F, 10.0F);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(15);
+
+		GUILayout.EndArea ();
+
+		if(GUI.changed){
+			sineWaveModel.modelData["amplitude"] = amplitude;
+			sineWaveModel.modelData["speed"] = speed;
+			sineWaveModel.modelData["wavelength"] = wavelength;
+		}
 	}
 		
 	public float GetWaveYPos(float x_coord, float z_coord) {
 		float y_coord = 0f;
-
-
 
 		if (sineWaveModel.modelData["continuousBool"] == 1f) {
 			y_coord += sineWaveModel.modelData["amplitude"] * Mathf.Sin ((Time.time * sineWaveModel.modelData["speed"] + z_coord) / sineWaveModel.modelData["wavelength"]) + sineWaveModel.modelData["bias"];
@@ -32,4 +86,18 @@ public class WaveController : MonoBehaviour {
 
 		return y_coord;
 	}	
+
+	private Texture2D MakeTex(int width, int height, Color col)
+	{
+		Color[] pix = new Color[width * height];
+
+		for (int i = 0; i < pix.Length; i++)
+			pix[i] = col;
+
+		Texture2D result = new Texture2D(width, height);
+		result.SetPixels(pix);
+		result.Apply();
+
+		return result;
+	}
 }
