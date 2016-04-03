7ad1e02d9cbe309b14826ff36b299f002365e0bd
jacklp <jackleonprior@gmail.com>
2016-04-03 18:07:23 +0100

    added textures to glass and water. added config file to resources folder. parsed config file in SineWaveModel. Instantiated SineWaveModel inside wave controller.

---
+++ Assets/Scripts/SineWaveModel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+
+public class SineWaveModel {
+
+	public Dictionary<string, float> modelData;
+
+	public void buildModel () {
+
+		modelData = new Dictionary<string, float> ();
+		TextAsset asset = (TextAsset)Resources.Load("config", typeof(TextAsset));
+		string element = "";
+
+		if(asset != null)
+		{
+			XmlTextReader reader = new XmlTextReader(new StringReader(asset.text));
+			while(reader.Read())
+			{
+
+				if (reader.NodeType == XmlNodeType.Element) {
+					element = reader.GetAttribute("type");
+
+				} else if (reader.NodeType == XmlNodeType.Text) {
+					modelData.Add(element, float.Parse (reader.Value));
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -3,63 +3,32 @@ using System.Collections;
 using System.Xml;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 public class WaveController : MonoBehaviour {
 
-	public float scale = 0.1f;
-	public float speed = 1.0f;
-	//The width between the wave peaks
-	public float waveDistance = 0.2f;
-	//Noise parameters
-	public float noiseStrength = 1f;
-	public float noiseWalk = 1f;
+	SineWaveModel sineWaveModel;
 
-	/*void Start(){
+	void Start(){
+		sineWaveModel = new SineWaveModel ();
+		sineWaveModel.buildModel ();
+	}
 		
-		File configs = Resources.Load
-		using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
-		{
-			XmlWriterSettings ws = new XmlWriterSettings();
-			ws.Indent = true;
-			using (XmlWriter writer = XmlWriter.Create(output, ws))
-			{
-
-				// Parse the file and display each of the nodes.
-				while (reader.Read())
-				{
-					switch (reader.NodeType)
-					{
-					case XmlNodeType.Element:
-						writer.WriteStartElement(reader.Name);
-						break;
-					case XmlNodeType.Text:
-						writer.WriteString(reader.Value);
-						break;
-					case XmlNodeType.XmlDeclaration:
-					case XmlNodeType.ProcessingInstruction:
-						writer.WriteProcessingInstruction(reader.Name, reader.Value);
-						break;
-					case XmlNodeType.Comment:
-						writer.WriteComment(reader.Value);
-						break;
-					case XmlNodeType.EndElement:
-						writer.WriteFullEndElement();
-						break;
-					}
-				}
-
-			}
-		}
-		OutputTextBlock.Text = output.ToString();
-	}*/
-
 	public float GetWaveYPos(float x_coord, float z_coord) {
 		float y_coord = 0f;
-	
-		y_coord += Mathf.Sin((Time.time * speed + z_coord) / waveDistance) * scale;
 
-		//add noise to make it more realistic
-		y_coord += Mathf.PerlinNoise(x_coord + noiseWalk, z_coord + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+
+
+		if (sineWaveModel.modelData["continuousBool"] == 1f) {
+			y_coord += sineWaveModel.modelData["amplitude"] * Mathf.Sin ((Time.time * sineWaveModel.modelData["speed"] + z_coord) / sineWaveModel.modelData["wavelength"]) + sineWaveModel.modelData["bias"];
+		} else {
+			// TODO: check time for how long left til it resets
+			//sineWaveModel.modelData["timePeriod"]
+		}
+
+		if (sineWaveModel.modelData["noiseBool"] == 1f) {
+			y_coord += Mathf.PerlinNoise (x_coord + sineWaveModel.modelData["noiseWalk"], z_coord + Mathf.Sin (Time.time * 0.1f)) * sineWaveModel.modelData["noiseStrength"];
+		}
 
 		return y_coord;
 	}	
